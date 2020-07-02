b429c47507210bd025fbc345446e74f9d7780fd3
kongchu2 <qwerty111515@gmail.com>
2020-07-02 18:12:38 +0900

    Asset Up

---
+++ Assets/03.Scripts/BackGroundSroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundSroll : MonoBehaviour
+{
+    public float scrollSpeed = 0.15f;
+    Material myMaterial;
+
+    private void Start()
+    {
+        myMaterial = GetComponent<MeshRenderer>().material;
+    }
+    private void Awake()
+    {
+        Screen.SetResolution(828, 1792, true);
+    }
+    private void Update()
+    {
+        if(!PlayerController.isDie)
+        {
+            float newOffsetX = myMaterial.mainTextureOffset.x + scrollSpeed * Time.deltaTime;
+            Vector2 newOffset = new Vector2(newOffsetX, 0);
+
+            myMaterial.mainTextureOffset = newOffset;
+        }
+    }
+}
---
+++ Assets/03.Scripts/tutorial.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class kimotti : MonoBehaviour
+{
+    AsyncOperation InGameScene;
+    public GameObject tutorialUI;
+    private AudioSource myAudio;
+    
+    private void Start()
+    {
+        InGameScene = null;
+        myAudio = GetComponent<AudioSource>();
+        StartCoroutine(MoveScene());
+    }
+    IEnumerator MoveScene()
+    {
+        InGameScene = SceneManager.LoadSceneAsync(1);
+        InGameScene.allowSceneActivation = false;
+        while(!InGameScene.isDone)
+        {
+            yield return null;
+        }
+    }
+    public void LoadScene()
+    {
+        LoadingSceneManager.LoadScene("InGameScene");
+    }
+    private void Update()
+    {
+        if (tutorialUI.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            myAudio.Play();
+            tutorialUI.SetActive(false);
+        }
+    }
+    public void GameScene()
+    {
+        Debug.Log("clicked");
+        InGameScene.allowSceneActivation = true;
+    }
+    public void Tutorial()
+    {
+        tutorialUI.SetActive(true);
+    }
+}
